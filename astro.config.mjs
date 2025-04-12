97dd666b69c8d3697ede0ddcc096ee67ab1dfc29
Johannes Fürmann <fuermannj@gmail.com>
2025-04-12 02:32:22 +0200

    first shot calendar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 import svelte from "@astrojs/svelte";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +13,12 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [svelte()],
+  integrations: [svelte(), icon()],
+
+  env: {
+    schema: {
+      DIRECTUS_URL: envField.string({context: "server", access: "public"}),
+      DIRECTUS_TOKEN: envField.string({context: "server", access: "secret"})
+    }
+  }
 });
---
+++ src/util/directus.ts
@@ -0,0 +1,26 @@
+import { createDirectus, rest, staticToken } from "@directus/sdk"
+import { DIRECTUS_URL, DIRECTUS_TOKEN } from "astro:env/server"
+
+export interface Calendar {
+  id: string
+  status: 'draft' | 'published' | 'archived'
+  title: string
+  description: string
+  image: string
+  location: string
+  public: boolean
+  starts_at: string
+  ends_at: string
+}
+
+export interface Schema {
+  Calendar: Calendar[]
+}
+
+export default createDirectus<Schema>(
+  DIRECTUS_URL,
+).with(
+  staticToken(DIRECTUS_TOKEN),
+).with(
+  rest()
+)
