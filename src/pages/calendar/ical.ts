70b891b710ce2afab6728d8de191438bfcf93cc5
Yonggan <yonggan@obco.pro>
2025-05-14 20:26:50 +0200

    Format

--- src/pages/calendar/ical.ts
+++ src/pages/calendar/ical.ts
@@ -35,7 +35,11 @@ export const GET: APIRoute = async ({ request }) => {
       mapDirectus(request.headers.get("host")!, event),
     ),
   );
-  const ics = createEvents(directusEvents.map(event => mapDirectus(request.headers.get("host")!, event)));
+  const ics = createEvents(
+    directusEvents.map((event) =>
+      mapDirectus(request.headers.get("host")!, event),
+    ),
+  );
 
   if (ics.error) {
     console.error(ics.error);
