3785128514cb34d059f137abaca7e90436403ffd
Johannes Fürmann <fuermannj@gmail.com>
2025-07-26 22:27:56 +0200

    Move static assets to repo

--- src/util/spaceapi.ts
+++ src/util/spaceapi.ts
@@ -35,13 +35,12 @@ export async function getSpaceApi(): Promise<SpaceApi> {
         api_compatibility: ["14", "15"],
         space: "OpenLab Augsburg e.V.",
         url: "https://openlab-augsburg.de",
-        logo: "https://cms.openlab-augsburg.de/assets/f500c2b9-8df1-4cce-bc95-65db756cb351.png",
+        logo: "https://www.openlab-augsburg.de/static/logo-colour.png",
         state: {
           open: dto.SpaceStatus !== "CLOSED",
           icon: {
-            open: "https://cms.openlab-augsburg.de/assets/5acb8735-aef6-434b-afae-7d51fc1ebdd5.png",
-            closed:
-              "https://cms.openlab-augsburg.de/assets/c62c0dac-cc57-4c1e-b111-93c3b80fe3cb.png",
+            open: "https://www.openlab-augsburg.de/static/open.png",
+            closed: "https://www.openlab-augsburg.de/static/closed.png",
           },
         },
         location: {
